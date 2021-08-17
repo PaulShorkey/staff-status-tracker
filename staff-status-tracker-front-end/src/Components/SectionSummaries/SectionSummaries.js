71b50c3bb286769130812e6e51999a934f4e47fa
Paul Shorkey-Chacon <paul@Pauls-MacBook-Pro.local>
2021-08-17 16:54:23 -0400

    Section Summaries and 3 grid in dashboard

--- staff-status-tracker-front-end/src/Components/SectionSummaries/SectionSummaries.js
+++ staff-status-tracker-front-end/src/Components/SectionSummaries/SectionSummaries.js
@@ -1,33 +1,59 @@
-import * as React from 'react';
-import { DataGrid } from '@material-ui/data-grid';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-export default function SectionSummaries() {
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+  tableCell: {
+      padding: ""
+  }
+});
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('S1', 'Green', 'All of the people are ready'),
+  createData('S2', 'Yellow', 'No intelegence, we werent able to find anything'),
+  createData('S3', 'Red', 'We should use MDMP')
+];
+
+export default function DenseTable() {
+  const classes = useStyles();
+
   return (
-    <div style={{ height: 250, width: '100%' }}>
-      <DataGrid
-        columns={[{ field: 'shop' }, { field: 'color' }, {field: 'blurb'}]}
-        rows={[
-          {
-            id: 1,
-            username: '@MaterialUI',
-            age: 20,
-          },
-        ]}
-        rows={[
-          {
-            id: 2,
-            username: '@MaterialUI',
-            age: 20,
-          },
-        ]}
-        rows={[
-          {
-            id: 3,
-            username: '@MaterialUI',
-            age: 20,
-          },
-        ]}
-      />
-    </div>
+    <TableContainer component={Paper}>
+      <Table className={classes.table} size="small" aria-label="a dense table">
+        {/* <TableHead>
+          <TableRow>
+            <TableCell >Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead> */}
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row" style={{width:'10%'}}>
+                {row.name}
+              </TableCell>
+              <TableCell align="left" style={{width:'10%'}} >{row.calories}</TableCell>
+              <TableCell align="left" style={{width:'80%'}}>{row.fat}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
